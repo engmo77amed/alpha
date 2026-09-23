79a6c16936e91b36364eec3a109685c18a5504e9
engmo77amed <engmo77amed@hotmail.com>
2026-09-23 12:00:49 +0300

    Fix Compose RTL import

--- site-calculator/app/src/main/java/com/fayroz/sitecalculator/MainActivity.kt
+++ site-calculator/app/src/main/java/com/fayroz/sitecalculator/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.CompositionLocalProvider
